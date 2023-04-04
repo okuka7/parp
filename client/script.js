e5655e41642baaf5d7ace13122f268f6131e6b6c
taegu <qorghab123@gmail.com>
2023-04-04 18:19:28 +0900

    menu창 수정

--- client/script.js
+++ client/script.js
@@ -6,12 +6,9 @@ const iconClose = document.querySelector('.icon-close');
 registerLink.addEventListener('click',()=>{
 	wrapper.classList.add('active');
 		});
-
 loginLink.addEventListener('click', ()=> {
 	wrapper.classList.remove('active');
 		});
-
-
 btnPopup.addEventListener('click', ()=> {
 	wrapper.classList.add('active-popup');
 		});
