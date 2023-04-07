3ddbc1cffc52a9db4efe6630754b881dc86e8af4
taegu <qorghab123@gmail.com>
2023-04-07 18:14:11 +0900

    make login

--- client/script.js
+++ client/script.js
@@ -13,10 +13,20 @@ loginLink.addEventListener('click', ()=> {
 		});
 btnPopup.addEventListener('click', ()=> {
 	wrapper.classList.add('active-popup');
+window.addEventListener('click',function(e){
+	if(btnPopup.contains(e.target)){
+		wrapper.classList.add('active-popup')
+		console.log("dd");
+	}
+	else if(wrapper.contains(e.target)){
+		console.log("dd")
+	}
+	else{
+		console.log("aa");
+		wrapper.classList.remove('active-popup');
+	}
+});
 		});
 iconClose.addEventListener('click', ()=> {
 	wrapper.classList.remove('active-popup');
-		});
-ac.addEventListener('click',()=> {
-	console.log("ddd");
 		});
