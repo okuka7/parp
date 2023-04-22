b537a1b42ee4d941077376fe117de7de95107100
grasshopper42 <solite4686@gmail.com>
2023-04-22 14:18:12 +0900

    fix: server port change

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
